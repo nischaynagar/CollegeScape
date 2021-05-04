b53a86e08e487f931dd7976730b899977d4516fc
nischaynagar <66030748+nischaynagar@users.noreply.github.com>
2021-05-04 22:03:19 +0530

    first commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,8 @@
 import React from "react";
+import design from "../Components/HomeComponent/homeComponent.module.css";
 
 function Home() {
-  return <div>Welcome to Home</div>;
+  return <div className={design.main}>Welcome to Home</div>;
 }
 
 export default Home;
