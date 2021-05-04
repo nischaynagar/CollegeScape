fcfff591c94b8f565b38fbe4b2edf204517d0840
nischaynagar <66030748+nischaynagar@users.noreply.github.com>
2021-05-04 22:06:28 +0530

    heading changed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 import design from "../Components/HomeComponent/homeComponent.module.css";
 
 function Home() {
-  return <div className={design.main}>Welcome to Home</div>;
+  return <div className={design.main}>Welcome</div>;
 }
 
 export default Home;
