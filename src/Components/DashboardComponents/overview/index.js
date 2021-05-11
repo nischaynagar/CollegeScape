a8494655f961e088164f674d749a354f69cba7e2
nischaynagar <66030748+nischaynagar@users.noreply.github.com>
2021-05-11 18:24:36 +0530

    dashboard

--- src/Components/DashboardComponents/overview/index.js
+++ src/Components/DashboardComponents/overview/index.js
@@ -13,30 +13,6 @@ import mainpic from "./images/mainpic.svg";
 function Appbox() {
   return (
     <>
-      <div className={design.leftbar}>
-        <div className={design.profile}>
-          <img src={mainpic} alt="hello" className={design.mainphoto}></img>
-        </div>
-        <p className={design.profileName}>Chodu Bhole</p>
-        <div className={design.choices}>
-          <div className={design.option}>
-            <img src={homeicon} alt="hello"></img>
-            <p className={design.mainoption}>Overview</p>
-          </div>
-          <div className={design.option}>
-            <img src={stats} alt="hello"></img>
-            <p className={design.optionnames}>Statistics</p>
-          </div>
-          <div className={design.option}>
-            <img src={profile} alt="hello"></img>
-            <p className={design.optionnames}>Profile</p>
-          </div>
-          <div className={design.option}>
-            <img src={admin} alt="hello"></img>
-            <p className={design.optionnames}>Admin</p>
-          </div>
-        </div>
-      </div>
       <div className={design.overview}>
         <div className={design.searchmenu}>
           <img src={searchbar} alt="hello"></img>
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Sidebar from "../Components/DashboardComponents/sidebar/index.js";
 import dashboardoverview from "../Components/DashboardComponents/overview/index.js";
 import dashboardstats from "../Components/DashboardComponents/statistics/index.js";
 import design from "../Components/DashboardComponents/statistics/styles.module.css";
@@ -8,6 +9,7 @@ function Dashboard() {
   return (
     <div>
       <div className={design.ContainerBox}>
+        <Sidebar />
         <Router>
           <Switch>
             <Route
