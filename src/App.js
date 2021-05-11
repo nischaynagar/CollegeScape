aa1accb4f9e62cd7ff55d5388db4a3332ad4592a
Rahul Roy <70642284+Rahul171201@users.noreply.github.com>
2021-05-11 17:53:57 +0530

    dashboardfinal

--- src/App.js
+++ src/App.js
@@ -2,20 +2,36 @@ import React from "react";
 import "./App.css";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import signup from './Components/LoginComponents/index.js';
-import dashboard from './Components/DashboardComponents/overview/index.js';
-import design from './styles.module.css';
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
+import dashboardoverview from './Components/DashboardComponents/overview/index.js';
+import dashboardstats from './Components/DashboardComponents/statistics/index.js';
+
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: [
+      'Montserrat',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif'
+    ].join(','),
+  }
+});
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <div className={design.LoginBox}>
-          <Route path='/' exact component={dashboard}></Route>
-        </div>
-      </Switch>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Switch>
+          <Route path="/login" exact component={Login}></Route>
+          <Route path="/" exact component={Home}></Route>
+          <Route path="/dashboard/overview" exact component={dashboardoverview}></Route>
+          <Route path="/dashboard/statistics" exact component={dashboardstats}></Route>
+        </Switch>
+      </Router>
+    </ThemeProvider>
   );
 }
 
