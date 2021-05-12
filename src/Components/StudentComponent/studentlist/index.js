9b04610a82b5401b30807c30078ed79f404f7c70
nischaynagar <66030748+nischaynagar@users.noreply.github.com>
2021-05-13 00:50:12 +0530

    css fix

--- src/Components/StudentComponent/studentlist/index.js
+++ src/Components/StudentComponent/studentlist/index.js
@@ -14,6 +14,7 @@ const StyledButton = withStyles({
     color: "white",
     height: 48,
     padding: "0 30px",
+    outline: "none !important",
   },
   label: {
     textTransform: "capitalize",
@@ -30,26 +31,24 @@ function StudentList() {
         </div>
         <div className={design.heading}>
           <p className={design.title}>Students</p>
-          <StyledButton className={design.addbutton}>
-            Add New Student
-          </StyledButton>
+          <StyledButton>Add New Student</StyledButton>
         </div>
         <div className={design.studentbranch}>
           <p className={design.branchname}>BTech IT</p>
           <div className={design.studentbox}>
             <img src={mainpic} alt="hello" className={design.photo}></img>
             <p className={design.id}>IIT2019232</p>
-            <p className={design.name}>Bineet Memers</p>
+            <p className={design.name}>Bineet Wagh</p>
           </div>
           <div className={design.studentbox}>
             <img src={mainpic} alt="hello" className={design.photo}></img>
-            <p className={design.id}>IIT2019232</p>
-            <p className={design.name}>Goti Warriors</p>
+            <p className={design.id}>IIT2019198</p>
+            <p className={design.name}>Nischay Nagar</p>
           </div>
           <div className={design.studentbox}>
             <img src={mainpic} alt="hello" className={design.photo}></img>
-            <p className={design.id}>IIT2019232</p>
-            <p className={design.name}> Bholu</p>
+            <p className={design.id}>IIT2019194</p>
+            <p className={design.name}>Rahul Rai</p>
           </div>
         </div>
       </div>
