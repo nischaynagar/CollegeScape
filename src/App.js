168b3a2f2c989a9e3e55bceaee270fcaa3221913
nischaynagar <66030748+nischaynagar@users.noreply.github.com>
2021-05-13 15:37:36 +0530

    useercontext

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Dashboard from "./pages/Dashboard";
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import { createMuiTheme } from "@material-ui/core/styles";
 import { ThemeProvider } from "@material-ui/styles";
+import { useUser } from "./contexts/user";
 
 const theme = createMuiTheme({
   typography: {
@@ -20,6 +21,18 @@ const theme = createMuiTheme({
 });
 
 function App() {
+  const [state, dispatch] = useUser();
+
+  useEffect(() => {
+    const user = localStorage.getItem("user");
+    if (user) {
+      dispatch({
+        type: "SET_USER",
+        user: JSON.parse(user),
+      });
+    }
+  }, [dispatch]);
+
   return (
     <ThemeProvider theme={theme}>
       <Router>
