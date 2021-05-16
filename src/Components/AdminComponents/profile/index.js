787d3dd6a037e903b343020026fd1474d7641b79
nischaynagar <66030748+nischaynagar@users.noreply.github.com>
2021-05-16 17:37:05 +0530

    css

--- src/Components/AdminComponents/profile/index.js
+++ src/Components/AdminComponents/profile/index.js
@@ -1,17 +1,21 @@
 import React, { useState } from "react";
-import { HashRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  useHistory,
+} from "react-router-dom";
 import "../../../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import searchbar from "./images/searchbar.svg";
 import arrow from "./images/arrow.svg";
 import mainpic from "./images/mainpic.svg";
-import design from './profile.module.css'
+import design from "./profile.module.css";
 import Button from "@material-ui/core/Button";
 import { withStyles } from "@material-ui/core/styles";
-import TextField from '@material-ui/core/TextField';
-import { makeStyles } from '@material-ui/core/styles';
+import TextField from "@material-ui/core/TextField";
+import { makeStyles } from "@material-ui/core/styles";
 import { styled } from "@material-ui/core/styles";
 import mButton from "@material-ui/core/Button";
-
 
 const ChangesButton = styled(mButton)({
   fontSize: "1.3vw",
@@ -22,8 +26,7 @@ const ChangesButton = styled(mButton)({
   textTransform: "none",
   fontFamily: "Montserrat",
   // transition: "all 0.9s ease-in-out",
-  background:
-    "#834BFF",
+  background: "#834BFF",
   // border: 0,
   // borderRadius: 3,
   boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.23)",
@@ -31,43 +34,44 @@ const ChangesButton = styled(mButton)({
   height: 48,
   padding: "0 30px",
   "&:hover": {
-    background:
-      "#C0A4FF",
+    background: "#C0A4FF",
   },
 });
 
 const useStyles = {
-    margin: '1rem 3rem',
-    '& .MuiTextField-root': {
-      width: '25ch',
-    }
+  margin: "1rem 3rem",
+  "& .MuiTextField-root": {
+    width: "25ch",
+  },
 };
-
 
 const firstName = "Rahul";
 const email = "rahulrai@orissa.com";
 const lastName = "Rai";
 const mobile = "+91692437294";
 
-
-
 function CurrentAdminProfile() {
+  const history = useHistory();
+
+  const handleGoBack = () => {
+    history.push("./");
+  };
+
   return (
     <>
       <div className={design.overview}>
         <div className={design.headingWrapper}>
           <div className={design.heading}>
             <p className={design.title}>Profile</p>
-            <div className={design.moveBackWrapper}>
-              <img className={design.moveBackImg} src={arrow} alt="move back"/>
-              <h4 className={design.moveBackText} >Move Back</h4>
+            <div className={design.moveBackWrapper} onClick={handleGoBack}>
+              <img className={design.moveBackImg} src={arrow} alt="move back" />
+              <h4 className={design.moveBackText}>Move Back</h4>
             </div>
           </div>
         </div>
         <div className={design.detailsWrapperMain}>
-        <img className={design.profileImg} src={mainpic} alt="student"/>
-        <div className={design.profileDetailsWrapper}>
-        
+          <img className={design.profileImg} src={mainpic} alt="student" />
+          <div className={design.profileDetailsWrapper}>
             <div className={design.col1}>
               <TextField
                 style={useStyles}
@@ -104,8 +108,10 @@ function CurrentAdminProfile() {
                 variant="outlined"
               />
             </div>
-        </div>
-        <ChangesButton className={design.changesButton}>Apply Changes</ChangesButton>
+          </div>
+          <ChangesButton className={design.changesButton}>
+            Apply Changes
+          </ChangesButton>
         </div>
       </div>
     </>
